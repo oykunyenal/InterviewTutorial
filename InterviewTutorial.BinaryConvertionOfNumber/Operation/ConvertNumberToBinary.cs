f7a080c18eed549211058672f57cf7407105c55b
OYENAL <OYENAL@DESKTOP-9PDS8TP>
2022-12-30 14:25:53 +0300

    binary manipulations for number

---
+++ InterviewTutorial.BinaryConvertionOfNumber/Operation/ConvertNumberToBinary.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace InterviewTutorial.BinaryConvertionOfNumber.Operation
+{
+    /// <summary>
+    /// Class has its own method to take int number as parameter
+    /// </summary>
+    internal class ConvertNumberToBinary
+    {
+        /// <summary>
+        /// Send a number to convert its binary representation
+        /// </summary>
+        /// <param name="number"></param>
+        /// <returns></returns>
+        public string ConvertNumberToBinaryRepresentation(int number)
+        {
+            return Convert.ToString(number, 2);
+        }
+
+        public int GetCountOfZerosInString(string binaryRep)
+        {
+            return binaryRep.Count(c => c == '0');
+        }
+
+        public int GetCountOfOnesInString(string binaryRep)
+        {
+            return binaryRep.Count(c => c == '1');
+        }
+
+    }
+}
---
+++ InterviewTutorial.BinaryConvertionOfNumber/Program.cs
@@ -0,0 +1,15 @@
+﻿using InterviewTutorial.BinaryConvertionOfNumber.Operation;
+
+ConvertNumberToBinary ConvertNumberToBinary = new ConvertNumberToBinary();
+
+string result = ConvertNumberToBinary.ConvertNumberToBinaryRepresentation(20);
+
+Console.WriteLine("Binary Representation Of Number : " + result);
+
+int resultForZerosCount = ConvertNumberToBinary.GetCountOfZerosInString(result);
+
+Console.WriteLine("How many zeros in binary string : " + resultForZerosCount);
+
+int resultForOnesCount = ConvertNumberToBinary.GetCountOfOnesInString(result);
+
+Console.WriteLine("How many ones in binary string : " + resultForOnesCount);
